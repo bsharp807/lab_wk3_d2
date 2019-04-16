c08de1cdf69fb3d23d07e10d3160f3e41ff83c1d
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-04-16 14:32:05 +0100

    final issue

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
-})
+
+  const form = document.querySelector('#new-item-form');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const newList = document.createElement('ul');
+
+    const l1 = document.createElement('li');
+    const l2 = document.createElement('li');
+    const l3 = document.createElement('li');
+    l1.textContent = event.target.title.value;
+    l2.textContent = event.target.author.value;
+    l3.textContent = event.target.category.value;
+    l1.classList = 'titleClass';
+    l2.classList = 'authorClass';
+    l3.classList = 'categoryClass';
+    newList.appendChild(l1);
+    newList.appendChild(l2);
+    newList.appendChild(l3);
+    const readingList = document.querySelector('#reading-list');
+    readingList.appendChild(newList);
+    form.reset();
+  });
+
+  const button = document.querySelector('#delete-all');
+
+  button.addEventListener('click', () => {
+    const readingList = document.querySelector('#reading-list');
+    const children = document.querySelectorAll('ul');
+    for(child of children){
+      readingList.removeChild(child);
+    };
+  });
+});
